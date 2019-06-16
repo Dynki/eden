94b498139e6dde5d731563db9721df8ffb380020
Dean Selvey <deanselvey@gmail.com>
2019-06-16 22:01:24 +0100

    Updates to images etc

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,38 @@
 import '../styles/main.scss';
 import * as $ from 'jquery';
 
-$("#btn-services").click(function() {
+$("#btn-services").on('touchstart click', function() {
     $('html,body').animate({
         scrollTop: $("#services-section").offset().top},
         'slow');
 });
 
-$("#sh-services-btn").click(function() {
+$("#sh-services-btn").on('touchstart click',function() {
     $('html,body').animate({
         scrollTop: $("#services-section").offset().top},
         'slow');
 });
 
-$("#small-services-btn").click(function() {
+$("#small-services-btn").on('touchstart click',function() {
     $('html,body').animate({
         scrollTop: $("#services-section").offset().top},
         'slow');
 });
 
-$("#mp-btn").click(function() {
+$("#mp-btn").on('touchstart click',function() {
     $('html,body').animate({
         scrollTop: $("#sm-section").offset().top},
+        'slow');
+});
+
+$("#nails-btn").on('touchstart click',function() {
+    $('html,body').animate({
+        scrollTop: $("#nails-section").offset().top},
+        'slow');
+});
+
+$("#find-us-btn, #btn-find-us, #btn-phonenumber,  #book-appt-sm-btn, #book-appt-med-btn, #book-appt-lrg-btn, #ask-questions-btn").on('touchstart click',function() {
+    $('html,body').animate({
+        scrollTop: $("#call-us-btn").offset().top},
         'slow');
 });
